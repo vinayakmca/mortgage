7606b6b91d65c2955bc7a882e276cf7de703e5db
vinayak <vinayakdesaimca@gmail.com>
2019-07-12 10:34:14 +0530

    add src and pom file

---
+++ src/test/java/com/hcl/mortgage/MortgageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.mortgage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MortgageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
